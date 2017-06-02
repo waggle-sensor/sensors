ef460e0aa11afe9e640bb28ce22cc702ae0afe82
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-06-02 15:05:00 -0500

    wrote framing read / write

--- src/main.ino
+++ src/main.ino
@@ -4,13 +4,54 @@
 #include "scanner.h"
 #include "stringutils.h"
 
+const char *ERROR_INVALID_ARGS = "invalid args";
+const char *ERROR_NOT_IMPLEMENTED = "not implemented";
+const char *ERROR_TIMED_OUT = "timed out";
+
 const char *version = "coresense 4.0.0";
 char inputbuf[256];
 int inputlen;
 OneWire ds(48);
 
-const char *ERROR_INVALID_ARGS = "invalid args";
-const char *ERROR_NOT_IMPLEMENTED = "not implemented";
+// const char *base16[16] = "0123456789ABCDEF";
+//
+// byte value16(char c) {
+//     if ('0' <= c && c <= '9') {
+//         return c - '0';
+//     }
+//
+//     if ('A' <= c && c <= 'F') {
+//         return c - 'A';
+//     }
+//
+//     if ('a' <= c && c <= 'f') {
+//         return c - 'a';
+//     }
+//
+//     return 0xFF;
+// }
+//
+// void encode16(const byte *b, char *s, int n) {
+//     for (int i = 0; i < n; i++) {
+//         *s++ = base16[(b[i] >> 4) & 0xF];
+//         *s++ = base16[(b[i] >> 0) & 0xF];
+//     }
+// }
+//
+// const char *decode16(const char *s, byte *b) {
+//     while (*s != '\0') {
+//         byte h = value16(*s++);
+//         byte l = value16(*s++);
+//
+//         if (h == 0xFF || l == 0xFF) {
+//             return "invalid symbol";
+//         }
+//
+//         *b++ = (h << 4) | l;
+//     }
+//
+//     return NULL;
+// }
 
 const char *commandVersion(int argc, const char **argv) {
     SerialUSB.println(version);
@@ -31,6 +72,7 @@ const char *commandID(int argc, const char **argv) {
         return "failed crc";
     }
 
+    // print hex string
     for (int i = 0; i < 8; i++) {
         SerialUSB.print((mac[i] >> 4) & 0x0F, HEX);
         SerialUSB.print((mac[i] >> 0) & 0x0F, HEX);
@@ -78,6 +120,7 @@ const char *commandI2CBegin(int argc, const char **argv) {
 
     int addr = atoi(argv[1]);
     Wire.beginTransmission(addr);
+
     return NULL;
 }
 
@@ -87,14 +130,56 @@ const char *commandI2CEnd(int argc, const char **argv) {
 }
 
 const char *commandI2CReq(int argc, const char **argv) {
-    return ERROR_NOT_IMPLEMENTED;
+    if (argc != 3) {
+        return ERROR_INVALID_ARGS;
+    }
+
+    int addr = atoi(argv[1]);
+    int count = atoi(argv[2]);
+
+    Wire.requestFrom(addr, count);
+
+    return NULL;
 }
 
 const char *commandI2CRead(int argc, const char **argv) {
-    return ERROR_NOT_IMPLEMENTED;
+    byte data[64];
+
+    if (argc != 2) {
+        return ERROR_INVALID_ARGS;
+    }
+
+    int count = atoi(argv[1]);
+
+    unsigned long startTime = millis();
+
+    for (int i = 0; i < count; i++) {
+        while (Wire.available() == 0) {
+            if (millis() - startTime > 5000) {
+                return ERROR_TIMED_OUT;
+            }
+        }
+
+        data[i] = Wire.read();
+    }
+
+    // print hex string
+    for (int i = 0; i < count; i++) {
+        SerialUSB.print((data[i] >> 4) & 0x0F, HEX);
+        SerialUSB.print((data[i] >> 0) & 0x0F, HEX);
+    }
+
+    SerialUSB.println();
+
+    return NULL;
 }
 
 const char *commandI2CWrite(int argc, const char **argv) {
+    // byte data[64];
+    //
+    // for (const char *s = argv[1], byte *b = data; *s != '\0'; s++, b++) {
+    // }
+
     return ERROR_NOT_IMPLEMENTED;
 }
 
--- src/sender.cpp
+++ src/sender.cpp
@@ -1,15 +1,39 @@
 #include "sender.h"
 #include <Arduino.h>
 
-const byte BBEGIN = 0xFC;
-const byte BEND = 0xFE;
-const byte BESCAPE = 0xCC;
+const byte BBEGIN = 0xFE;
+const byte BEND = 0xFC;
+const byte BESCAPE = 0xFD;
 
 bool ControlByte(byte b) {
-    return b == BBEGIN || b == BEND || b == BESCAPE;
+    return (b == BBEGIN) || (b == BEND) || (b == BESCAPE);
 }
 
-void SendFrame(byte *b, int n) {
+class Framer {
+public:
+
+    Framer();
+    const char *Err() const;
+    void WriteFrame(byte *b, int n);
+    int ReadFrame(byte *b, int max);
+
+private:
+
+    byte ReadByte();
+    byte ReadEscapedByte();
+
+    const char *err;
+};
+
+Framer::Framer() {
+    err = 0;
+}
+
+const char *Framer::Err() const {
+    return err;
+}
+
+void Framer::WriteFrame(byte *b, int n) {
     byte crc = 0;
 
     SerialUSB.write(BBEGIN);
@@ -25,4 +49,76 @@ void SendFrame(byte *b, int n) {
 
     SerialUSB.write(crc);
     SerialUSB.write(BEND);
+}
+
+int Framer::ReadFrame(byte *data, int max) {
+    int size = 0;
+
+    // wait for frame start
+    while (1) {
+        byte b = ReadByte();
+
+        if (err != NULL) {
+            return;
+        }
+
+        if (b == BBEGIN) {
+            break;
+        }
+    }
+
+    // wait for data / end bytes.
+    while (1) {
+        if (size > max) {
+            err = "frame too big";
+            break;
+        }
+
+        byte b = ReadByte();
+
+        if (err != NULL) {
+            break;
+        }
+
+        if (b == BEND) {
+            break;
+        }
+
+        if (b == BESCAPE) {
+            data[size++] = ReadEscapedByte();
+        } else {
+            data[size++] = b;
+        }
+    }
+
+    return size;
+}
+
+byte Framer::ReadByte() {
+    unsigned long start = millis();
+
+    while (SerialUSB.available() == 0) {
+        if (millis() - start > 5000) {
+            err = "read timeout";
+            return 0;
+        }
+    }
+
+    return SerialUSB.read();
+}
+
+byte Framer::ReadEscapedByte() {
+    byte b = ReadByte();
+
+    if (err != NULL) {
+        return 0;
+    }
+
+    // control bytes should never follow escape byte.
+    if (ControlByte(b)) {
+        err = "invalid escaped byte";
+        return 0;
+    }
+
+    return b ^ 0x20;
 }
