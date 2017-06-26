59abea0dce688fada9bb13b7359ac3eaa410ba46
spark <park708@purdue.edu>
2017-06-26 10:57:29 -0500

    remove old files/folder

--- src/hex.h
+++
@@ -1,4 +0,0 @@
-typedef unsigned char byte;
-
-int EncodeHex(const byte *src, char *dst, int size);
-int DecodeHex(const char *src, byte *dst);
--- src/stringutils.h
+++
@@ -1,6 +0,0 @@
-bool startswith(const char *s, const char *p);
-bool matches(const char *s1, const char *s2);
-char toLower(char c);
-char toUpper(char c);
-int parseInt(const char *s);
-int parseHex(const char *s);
