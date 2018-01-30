3a907039e6f72d76b0f6ef4ee15059d2fda11940
sager <park708@purdue.edu>
2018-01-30 17:40:50 -0600

    length issue

--- v4.1/integrated/firmware/Sensor08.ino
+++ v4.1/integrated/firmware/Sensor08.ino
@@ -31,7 +31,7 @@ void ReadSensor08(byte *sensorReading, int *readingLength)
 {
 	int buff = 0;
 
-	for (int i = 0; i < 64; i++)
+	for (int i = 0; i < 63; i++)
 	{
 		buff = analogRead(SPV_RAW_MIC_PIN);
 		sensorReading[i * 2] = (buff >> 8) & 0xFF;
