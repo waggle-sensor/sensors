27ec19edac92dd1969421985c80bfa1fd19d0604
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-03-22 09:25:39 -0500

    cleanup

--- vibration_test/vibration_test.py
+++ vibration_test/vibration_test.py
@@ -1,15 +1,16 @@
 # for mma8452q
 import sys
-from serial import Serial
-from io import BytesIO
 import time
+from serial import Serial
 
 
 MSG_FRAME = 0x7e
 MSG_ESC = 0x7d
 
 
-def decode_frame(reader, writer):
+def get_frame(reader):
+    data = []
+
     while True:
         b = reader.read(1)
         if len(b) == 0:
@@ -18,26 +19,22 @@ def decode_frame(reader, writer):
             break
         if b[0] == MSG_ESC:
             b = reader.read(1)
-            writer.write(bytes([b[0] ^ 0x20]))
+            data.append(bytes([b[0] ^ 0x20]))
         else:
-            writer.write(bytes([b[0]]))
-
-
-def get_frame_data(reader):
-    writer = BytesIO()
-    decode_frame(reader, writer)
-    return writer.getvalue()
+            data.append(bytes([b[0]]))
+    
+    return b''.join(data)
 
 
 with Serial(sys.argv[1], baudrate=115200, timeout=180) as ser:
     # sync to incoming frames
-    get_frame_data(ser)
+    get_frame(ser)
 
     total = 0
     start = time.time()
 
     while True:
-        data = get_frame_data(ser)
+        data = get_frame(ser)
         total += 1
         rate = total / (time.time() - start)
         print(data.hex(), round(rate))
