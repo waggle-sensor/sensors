b893257b7cecf348128e6520b6a860a0fa0b7726
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-03-26 17:27:27 -0500

    update

--- perf/firmware/firmware.ino
+++ perf/firmware/firmware.ino
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 
 void setup() {
-  SerialUSB.begin(115200);
+  // Select highest rate.
+  // This seems to be able to dramatically exceed standard rates. I've seen rates into the 5-6 MB/s range.
+  SerialUSB.begin(0);
 }
 
 const byte data[256] = {0};
